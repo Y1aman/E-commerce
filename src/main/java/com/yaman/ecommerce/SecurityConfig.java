e3d3133982d322536af42711963a0e64cff56eef
Yaman <yamana192@gmail.com>
2025-12-05 13:48:52 +0300

    Add Products page UI with Bootstrap card layout and ProductsController

--- src/main/java/com/yaman/ecommerce/SecurityConfig.java
+++ src/main/java/com/yaman/ecommerce/SecurityConfig.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +20,15 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user@example.com")
+                .password(passwordEncoder().encode("12345"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,6 +40,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
+                        .loginProcessingUrl("/login")   // 🔥 مهم جدًا
                         .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
